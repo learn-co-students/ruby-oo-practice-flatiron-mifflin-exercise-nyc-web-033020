6bbc7f329330f724ff2a86a2e99263e888325b74
Edgar <edgar.a.rivera08@gmail.com>
2020-04-05 13:53:13 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,45 @@
-class Employee
+require "pry"
 
+class Employee
+    attr_reader :name, :salary
+    attr_accessor :manager
+    @@all = []
+    def initialize(name, salary, manager = nil)
+        @name = name
+        @salary = salary
+        @manager = manager
+        Employee.all << self
+    end
+
+    def manager_name
+        self.manager.name
+    end
+
+    def self.paid_over (num)
+        Employee.all.select {|employee| employee.salary > num}
+    end
+
+    def self.find_by_department(string)
+
+        Manager.all.first {|managers| 
+        managers.department == string}
+    end
+    
+    def tax_bracket
+        result = []
+        max = self.salary + 100
+        min = self.salary - 100
+        # binding.pry
+        Employee.all.each do |employees|
+           result << employees.name if employees.salary < max && employees.salary > min 
+        end
+        result
+    end
+
+    def self.all
+        @@all
+    end
 end
+
+
+
--- run.rb
+++ run.rb
@@ -3,8 +3,18 @@ require_relative "lib/Employee"
 require 'pry'
 
 
+
 #Test your code here
 
-
-binding.pry
-puts "done"
+edgar = Manager.new("Edgar", "Paper", 26)
+manager1 = Manager.new("John", "Paper", 29)
+lei = Manager.new("Lei", "Paper", 25)
+manager2 = Manager.new("Guy", "Warehouse", 26)
+manager3 = Manager.new("Sam", "Paper", 26)
+eman = Employee.new("Emmanuel", 200, lei)
+employee1 = Employee.new("Melissa", 300, lei)
+employee2 = Employee.new("Carmen", 250, edgar)
+employee3 = Employee.new("Ashley", 500, lei)
+employee1.tax_bracket
+# binding.pry
+"done"
