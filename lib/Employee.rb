bfe2626f340cb9c572955cfeb77b493782d03da7
Taevon Jordan <nude.plus@Taevons-MacBook-Pro.local>
2020-04-05 20:49:07 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,23 @@
 class Employee
+@@all = []
+  attr_accessor :name, :salary, :manager
+
+  def initialize(name, salary=0, manager)
+    @name = name
+    @salary = salary
+    @manager = manager
+    @@all << self
+  end
+
+  def employee_name
+    p "Hi, my name is #{@name}"
+  end
+
+
+
+
+  def self.all
+   @@all
+  end
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,40 @@
 class Manager
+  attr_accessor :name, :age, :department
+  @@all = []
+  def initialize(name, age=0, department)
+    @name = name
+    @age = age
+    @department = department
+    Manager.all << self
+    @employees = []
+  end
+
+  def manager_name
+    p "Hi, my name is #{@name}!"
+  end
+
+  def manager_department
+    p "I oversee the #{@department} department."
+  end
+
+  def manager_employees
+    Employee.all.select do |employee|
+      employee.manager = self
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def employees
+    @employees
+  end
+
+  def hire_employee(name, salary)
+    Employee.new(name, salary, self)
+  end
+
+
 
 end
