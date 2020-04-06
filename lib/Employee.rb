1bf805f4c2347460395eda4b651a03cbefc89bd5
dkersten <dkersten1994@gmail.com>
2020-04-05 20:29:24 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,38 @@
 class Employee
 
+    attr_accessor :name, :salary, :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+
+        Employee.all << self
+    end
+
+    def manager_name
+        @manager
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(num)
+        new_array = []
+        Employee.all.each do |employee| 
+            if employee.salary > num
+                new_array << employee
+            end
+        end
+        new_array
+    end
+
+    def self.find_by_department(department)
+        # Employee.all.each do |employee|
+        #     employee.find(department)
+        # end
+    end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,48 @@
 class Manager
 
+    attr_accessor :name, :department, :age
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+
+        Manager.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    # not working
+    def employees
+        Employee.all.select do |employee|
+           employee.manager == self
+        end
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        Manager.all.map {|manager| manager.department}
+    end
+
+    # not working
+    def self.average_age
+        age_array = []
+        Manager.all.each do |manager|
+            manager.age << age_array
+        end
+
+        sum = 0
+        age_array.each do |num|
+            sum += num
+        end
+        sum / age_array.count
+    end
+
 end
